80ca392b5a0ee28a7a215462c768f0455afaee8d
Solant <runner62v6@gmail.com>
2015-09-03 16:28:31 +0300

    administrator permissions check added

--- windows/add_context_menu_option.bat
+++ windows/add_context_menu_option.bat
@@ -1,3 +1,13 @@
+@echo off
 REM Add "Share via cutehttpd" to context menu, run as administrator
-reg add "HKEY_CLASSES_ROOT\Directory\shell\Share via cutehttpd\command" /t REG_SZ /d "%~dp0\cutehttpd.exe %%1"
-reg add "HKEY_CLASSES_ROOT\Directory\Background\shell\Share via cutehttpd\command" /t REG_SZ /d "%~dp0\cutehttpd.exe %%V"
+
+REM Check Administrator permissions
+net session >nul 2>&1
+if %errorLevel% == 0 (
+  reg add "HKEY_CLASSES_ROOT\Directory\shell\Share via cutehttpd\command" /t REG_SZ /d "%~dp0\cutehttpd.exe %%1"
+  reg add "HKEY_CLASSES_ROOT\Directory\Background\shell\Share via cutehttpd\command" /t REG_SZ /d "%~dp0\cutehttpd.exe %%V"
+  echo Success. Context menu entry added.
+) else (
+  echo Error. Run this bat with Administrator permissions.
+)
+pause
